3e11921c71d481827d9936d45a75bdfd382260a8
Neil Lalonde <neillalonde@gmail.com>
2015-10-21 16:17:37 -0400

    restrict to html requests

--- lib/no_group_constraint.rb
+++ lib/no_group_constraint.rb
@@ -1,6 +1,7 @@
 class NoGroupConstraint
 
   def matches?(request)
+    return false unless request.format.html?
     path = request.fullpath.downcase
     group_id = path.split('/')[3]
     !Group.where(name: group_id).exists?
